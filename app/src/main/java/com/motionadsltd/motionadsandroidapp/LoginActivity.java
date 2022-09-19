21093e28b73eaa809bf1e654b087355afc8a7202
Nahid <nahidhasan24>
2022-09-19 15:13:17 +0600

    Initial commit

---
+++ app/src/main/java/com/motionadsltd/motionadsandroidapp/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.motionadsltd.motionadsandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/motionadsltd/motionadsandroidapp/SignActivity.java
@@ -0,0 +1,15 @@
+package com.motionadsltd.motionadsandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+    }
+}
